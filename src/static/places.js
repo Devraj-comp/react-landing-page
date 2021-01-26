9e481e1b933067487b92b574a56f5d30c1ef7cbb
devraj <you@example.com>
2021-01-26 14:36:47 +0545

    first commit

---
+++ src/static/places.js
@@ -0,0 +1,17 @@
+const places = [
+    {
+        title: "Maldives",
+        description: "The Maldives are home to some of the world's most ravishing islands, but it's a faraway from the setlement zones. So, aside that its a place for refreshment of mind and soul together...",
+        imageUrl: process.env.PUBLIC_URL + "assets/img-4.jpg",
+        time: 1500,
+    },
+
+    {
+        title: "Caribbean-Bay",
+        description: "Sees like destination from Pirates of Caribbean movies, you can have a realistic Jacksparrow characters in yourself with dead men telling no tells. The screams of capitan Salazar...",
+        imageUrl: process.env.PUBLIC_URL + "/assets/img-6.jpg",
+        time: 1500,
+    },
+];
+
+export default places;
